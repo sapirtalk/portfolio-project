56964736b6b7603e0a34fadba5b4595499a5d3b2
sapirtalk <sapirtalk@gmail.com>
2025-03-16 11:19:38 +0200

    analytics debug

--- src/components/GoogleAnalytics.jsx
+++ src/components/GoogleAnalytics.jsx
@@ -8,14 +8,19 @@ export default function GoogleAnalytics() {
       console.warn("Google Analytics Measurement ID is missing you wanker fix it!");
       return;
     }
+    else {
+      console.log("Google Analytics Measurement ID ready");
+    }
 
     // Ensure dataLayer is available
     window.dataLayer = window.dataLayer || [];
     function gtag() { window.dataLayer.push(arguments); }
 
+
     // Initialize Google Analytics
     gtag("js", new Date());
     gtag("config", GA_MEASUREMENT_ID);
+    console.log("Analytics initialized (I can see you! shhh , but no worries its only me lol)");
   }, []);
 
   return (
